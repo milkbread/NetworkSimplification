3af60b85a3157a6130de8964acd10fd45e18ca48
milkbread <milkbread@freenet.de>
2014-04-29 11:53:54 +0200

    Clean, comment and update readme.md

--- functions.js
+++ functions.js
@@ -1,10 +1,10 @@
 // Collapse the quadtree into an array of rectangles.
 function nodes(quadtree) {
-  var nodes = [];
-  quadtree.visit(function(node, x1, y1, x2, y2) {
-    nodes.push({x: x1, y: y1, width: x2 - x1, height: y2 - y1});
-  });
-  return nodes;
+	var nodes = [];
+	quadtree.visit(function(node, x1, y1, x2, y2) {
+	nodes.push({x: x1, y: y1, width: x2 - x1, height: y2 - y1});
+	});
+	return nodes;
 }
 
 // Add the selector for the number of points to remove (when a line was selected) and implement filtering
@@ -37,6 +37,9 @@ function addSimplificationSelector(id, path) {
 		})
 }
 
+// Transform all groups in relation to new values:
+// 		- 'groupScale'
+// 		- 'groupPositionX' & 'groupPositionY'
 function transformGroup() {
 	// Transform all groups
 	groups.forEach(function(group) {
@@ -51,59 +54,73 @@ function transformGroup() {
 	constrainingPoints.attr("r", 5 / groupScale);
 }
 
+// Action when mouse-movement has started
 function startMoving() {
+	// save the starting position of the mouse
 	var startingPosition = d3.mouse(this);
+	// add mouse-move event listener to svg-container
 	svg.on("mousemove", function() {
+		// get current position of the mouse
 		var currentPosition = d3.mouse(this);
 		groupPositionX = oldGroupPositionX + currentPosition[0] - startingPosition[0];
 		groupPositionY = oldGroupPositionY + currentPosition[1] - startingPosition[1];
+		// Apply group transformations for all groups
 		transformGroup();
 	});
 }
 
+// Action when the mouse-movement has stopped
 function stopMoving() {
+	// remove mouse-move event listener from the svg-container
 	svg.on("mousemove", null);
+	// Re-Define the globally defined old group position
 	oldGroupPositionX = groupPositionX;
 	oldGroupPositionY = groupPositionY;
 }
 
+// Change the global 'scale' on a mousewheel-drag-event
 function reScale() {
-		//detect the mousewheel event
-	if (d3.event.sourceEvent.type.indexOf("wheel") != -1 || d3.event.sourceEvent.type.indexOf("wheel") != -1){
+	// check if the drag-event is a mousewheel event
+	if (d3.event.sourceEvent.type.indexOf("wheel") != -1){
+		// init  a local variable for changed scale
   		var scaleChange = .1;
   		if (d3.event.sourceEvent.wheelDelta){
 			if (d3.event.sourceEvent.wheelDelta < 0){
 				scaleChange =  scaleChange * -1;
 			}
-		}else{
+		} else {
 			if (d3.event.sourceEvent.detail < 0){
 				scaleChange =  scaleChange * -1;
 			}
 		}
-		// Calculate the change of the scale and the new translation-parameter
+		// Re-Define the new global 'scale'
 		groupScale = groupScale + scaleChange;
-
+		// Apply group transformations for all groups
 		transformGroup();
 	}
 }
 
-function getExtent(elements) {
+// Get the extent of geoJSON input data
+function getExtent(geoJsonData) {
+	// !!!Works only for "LineString" currently!!!
 	var extent = [[181, 181], [-181, -181]];
-	elements.features.forEach(function(feature) {
-	feature.geometry.coordinates.forEach(function(point) {
-		// build the extent
-		if (point[0] < extent[0][0]) extent[0][0] = point[0];
-		if (point[1] < extent[0][1]) extent[0][1] = point[1];
-		if (point[0] > extent[1][0]) extent[1][0] = point[0];
-		if (point[1] > extent[1][1]) extent[1][1] = point[1];
+	geoJsonData.features.forEach(function(feature) {
+		feature.geometry.coordinates.forEach(function(point) {
+			// build the extent
+			if (point[0] < extent[0][0]) extent[0][0] = point[0];
+			if (point[1] < extent[0][1]) extent[0][1] = point[1];
+			if (point[0] > extent[1][0]) extent[1][0] = point[0];
+			if (point[1] > extent[1][1]) extent[1][1] = point[1];
 		});
 	});
 	return extent;
 }
 
-function getAllPoints(elements, projection) {
+// Get all points of geoJSON input data
+function getAllPoints(geoJsonData, projection) {
+	// !!!Works for 'Points' and 'LineStrings' currently!!!
 	var dataPoints = [];
-	elements.features.forEach(function(feature) {
+	geoJsonData.features.forEach(function(feature) {
 		if (feature.geometry.type === "LineString") {
 			feature.geometry.coordinates.forEach(function(point) {
 				dataPoints.push(projection(point));
@@ -121,7 +138,10 @@ function filterPoints(point, numberOfPoints) {
 	} else return point;
 }
 
+// Function to calculate the area of the triangle of one certain point
+// snippet taken from: http://bost.ocks.org/mike/simplify/
 function addTriangleSize(feature, projection) {
+	// !!!Works only for 'LineStrings' currently!!!
 	triangles = [];
 	var points = feature.coordinates
 	for (var i=1;i<points.length-1;i++){
@@ -129,7 +149,6 @@ function addTriangleSize(feature, projection) {
 		triangle = points.slice(i - 1, i + 2);
 		if (triangle[1][2] = area(triangle)) {
 			triangles.push(triangle);
-			// heap.push(triangle);
 		}
 	}
 	var result = feature.coordinates.map(function(lineString) {
@@ -138,7 +157,6 @@ function addTriangleSize(feature, projection) {
 			triangle = points.slice(i - 1, i + 2);
 			if (triangle[1][2] = area(triangle)) {
 				triangles.push(triangle);
-				// heap.push(triangle);
 			}
 		}
 
@@ -152,19 +170,32 @@ function addTriangleSize(feature, projection) {
 	return result;
 }
 
+// Calculate the area
 function area(t) {
   return Math.abs((t[0][0] - t[2][0]) * (t[1][1] - t[0][1]) - (t[0][0] - t[1][0]) * (t[2][1] - t[0][1]));
 }
 
+// Add a ranking to each point
+// Logic:
+// 			1. - push all triangle-areas into an array
+// 			2. - sort the array
+// 			3. - make an indexed object from the sorted array {id0: rank, id1: rank, ..., idN: rank}
+// 			4. - add the rank to each point (point[3]), by using the indexed object
 function rankAfterTriangles(feature) {
-	var triangles = feature.geometry.coordinates.map(function(d, i) {return {area: typeof d[2] !== "undefined" ? d[2] : -1, index: i};})
+	// 1:
+	var triangles = feature.geometry.coordinates.map(function(d, i) {
+		return {area: typeof d[2] !== "undefined" ? d[2] : -1, index: i};
+	})
+	// 2:
 	triangles.sort(function(a, b) {
 		return a.area - b.area;
 	});
+	// 3:
 	var trianglesObject = {};
 	triangles.forEach(function(d, rank) {
 		trianglesObject[d.index] = rank;
 	})
+	// 4:
 	feature.geometry.coordinates = feature.geometry.coordinates.map(function(point, i) {
 		point[3] = trianglesObject[i];
 		return point;
--- readme.md
+++ readme.md
@@ -7,3 +7,6 @@ Thanks for helping me:
 * [Projecting using transformation](http://stackoverflow.com/questions/20987535/plotting-points-on-a-map-with-d3)
 * [Add scrolling](http://thisismattmiller.com/blog/add-zoom-slider-to-d3-js/)
 * ['classed'-operator D3](https://groups.google.com/forum/#!topic/d3-js/NcvfRGo_TMc)
+
+* [get area of traingles - explanation][http://bost.ocks.org/mike/simplify/]
+* [get area of traingles - code][http://bost.ocks.org/mike/simplify/simplify.js]
